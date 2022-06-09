372544338f4bdc037856e27b2b3605a2b146611a
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-06-09 11:51:51 -0400

    seed expenses db done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+30.times do |i|
+  expense = Expense.create(
+    supplier: "Proveedor#{(i+1)}",
+    month: "#{["enero", "febrero", "marzo", "abril", "mayo", "junio"].sample}",
+    year: "2022",
+    document_type: "#{['Factura', 'Boleta'].sample}",
+    document_date: "05-05-2022",
+    category: "#{['Mantención', 'Reparación', 'Remuneraciones'].sample}",
+    amount: 1000 + i,
+    description: "Creado desde el seeds.rb",
+    payment_method: "#{['Transferencia', 'Cheque', 'Depósito', 'Efectivo'].sample}",
+    payment_number: "#{(100 + i)}"
+  )
+end
